fa91420567fbf6734d6552d7b3a9af04c0aaf36d
Adrià Ferrer Sanz <ferrer.sanz.adria@alumnat.copernic.cat>
2023-04-14 18:11:00 +0200

    Tasca 5A

---
+++ app/src/main/java/cat/copernic/clovis/Activity/Register.kt
@@ -0,0 +1,12 @@
+package cat.copernic.clovis.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.copernic.clovis.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
